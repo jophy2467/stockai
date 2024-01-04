d4e050eb43d7b33faefdfb4454dd4c46e8b0157b
jophy2467 <jophy2467@gmail.com>
2024-01-03 21:59:17 -0500

    New code

--- home.py
+++ home.py
@@ -23,15 +23,15 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 ######################################## tabs ########################################
-listTabs =["🧑‍🏭Junior AI", "🧑‍🎓College Ready", "📈 Math In Programming", "🔢 On-demand", "        "]
+listTabs =["About Me", "Projects I've Worked On", "Achievements and Awards", "Extracirricular Activties", "School Activities"]
 
 whitespace = 9
-st.markdown("#### 💡 Checkout our exciting programs!")
+st.markdown("#### Hi, I'm Jophy!")
 ## Fills and centers each tab label with em-spaces
 tabs = st.tabs([s.center(whitespace,"\u2001") for s in listTabs])
 
 with tabs[0]:
-    st.markdown("##### Junior AI Program")
+    st.markdown("##### About Me")
     style = """
     <style>
         .title {
@@ -136,7 +136,7 @@ with tabs[0]:
 
 #################################### college ready ####################################
 with tabs[1]:
-        st.markdown("##### College Ready")
+        st.markdown("##### Projects I've Worked On")
         # Program Overview
         st.markdown("<h2 style='font-size: 36px; margin-bottom: 0;'>AI Education Program for High School Students</h2>", unsafe_allow_html=True)
         st.markdown("<h3 style='font-size: 24px; margin-top: 0;'>Program Overview</h3>", unsafe_allow_html=True)
@@ -204,7 +204,7 @@ with tabs[1]:
 
 #################################### on-demand ####################################
 with tabs[3]:
-        st.markdown("##### Re-charge! On-deman")
+        st.markdown("##### Extracirricular Activities")
         # Program Overview
         st.markdown("<h2 style='font-size: 36px; margin-bottom: 0;'>On-demand, Per-Request Training and Consulting Program</h2>", unsafe_allow_html=True)
         st.markdown("<h3 style='font-size: 24px; margin-top: 0;'>Program Overview</h3>", unsafe_allow_html=True)
@@ -265,7 +265,7 @@ with tabs[3]:
 
 #################################### math and programming ####################################
 with tabs[2]:
-        st.markdown("##### Math in Programming")
+        st.markdown("##### Achievements and Awards")
         # Program Overview
         st.markdown("<h2 style='font-size: 36px; margin-bottom: 0;'>Programming and Math Together for K-12 Students</h2>", unsafe_allow_html=True)
         st.markdown("<h3 style='font-size: 24px; margin-top: 0;'>Program Overview</h3>", unsafe_allow_html=True)
@@ -315,3 +315,61 @@ with tabs[2]:
         st.markdown("""Our Tech Programming and Math Together program is designed to provide K-12 students with a unique and engaging learning experience. By combining programming and math, we aim to foster critical thinking, problem-solving, and creativity skills in our students. Our small class sizes and flexible schedule allow students to receive personalized attention and learn at their own pace. Our experienced instructors guide students through hands-on projects and real-world applications, giving them the skills and knowledge they need to succeed in a rapidly evolving tech industry.
 
 Whether your child is just starting out in their programming and math journey or looking to take their skills to the next level, our program has something to offer. We believe that by inspiring the next generation of tech innovators, we can help shape a better future for everyone.""")
+
+with tabs[4]:
+        st.markdown("##### School Activities")
+        # Program Overview
+        st.markdown("<h2 style='font-size: 36px; margin-bottom: 0;'>On-demand, Per-Request Training and Consulting Program</h2>", unsafe_allow_html=True)
+        st.markdown("<h3 style='font-size: 24px; margin-top: 0;'>Program Overview</h3>", unsafe_allow_html=True)
+
+        st.markdown("""
+        At our on-demand training program, we understand that everyone's learning needs are unique. That's why we offer a customizable and flexible approach to learning. With our program, you can choose the technology you want to learn and our experts will teach you based on your specific needs.
+        """)
+
+        # Program Details
+        st.markdown("<h3 style='font-size: 24px;'>How it works:</h3>", unsafe_allow_html=True)
+
+        # Goal
+        st.markdown("<h4 style='font-size: 18px;'>Goal</h4>", unsafe_allow_html=True)
+        st.markdown("""
+        - Our on-demand training program is designed to help you stay ahead of the curve in the rapidly-evolving field of technology, and to give you the skills and knowledge you need to succeed in your career.
+        - Whether you are looking to advance in your current job or explore new career opportunities, our program can help you achieve your goals.
+        - By participating in our program, you will join a community of like-minded learners who are passionate about technology and committed to lifelong learning.
+        """)
+
+        # Who
+        st.markdown("<h4 style='font-size: 18px;'>Who</h4>", unsafe_allow_html=True)
+        st.markdown("""
+        - Our on-demand training program is designed for individuals who are interested in expanding their knowledge and skills in the field of technology, including AI and other state-of-the-art technologies.
+        - Whether you are a student, a working professional, or simply someone who wants to stay ahead of the curve, our program is perfect for anyone who wants to learn and grow in their career.
+        - Our program is open to anyone, regardless of their prior experience or education level.
+        """)
+
+        # When we teach
+        st.markdown("<h4 style='font-size: 18px;'>When We Teach</h4>", unsafe_allow_html=True)
+        st.markdown("""
+        - Our program is offered on a rolling basis throughout the year
+        - Students can enroll at any time and complete the program at their own pace
+        - All materials and resources are available online, 24/7
+        """)
+
+        # what
+        st.markdown("<h4 style='font-size: 18px;'>What</h4>", unsafe_allow_html=True)
+        st.markdown("""
+        - Our program offers a wide range of topics to choose from, including AI, automation, main stream technologies, and expert help and consulting.
+        - With our program, you can choose which topic you want to learn and bid on the price you are willing to pay. Our experts will then work with you to design a personalized training program that meets your specific needs and goals.
+        - You will have access to high-quality learning materials, including videos, tutorials, and hands-on projects, all designed to help you master the topic of your choice.
+        """)
+
+        # how
+        st.markdown("<h4 style='font-size: 18px;'>How</h4>", unsafe_allow_html=True)
+        st.markdown("""
+        - Our program is entirely on-demand, which means you can learn at your own pace and on your own schedule.
+        - You will have access to our team of expert instructors, who will provide personalized guidance and support throughout your learning journey.
+        - Our program is designed to be interactive and engaging, with plenty of opportunities for hands-on learning and collaboration with other learners.
+        """)
+
+        # on-demand
+        st.markdown("<h4 style='font-size: 24px;'>On-demand, per-request or consulting</h4>", unsafe_allow_html=True)
+        st.markdown("""At our on-demand training program, we are committed to helping individuals and businesses succeed in the rapidly changing world of technology.
+        Whether it's AI, automation, web development, mobile app development, or expert help and consulting, we have the expertise and resources to help our customers achieve their learning goals.""")
